d34095245703e690dff19bc64d968a872d86d652
Karl <tekamkarl@gmail.com>
2022-10-13 12:52:40 -0700

    prints something on the screen by using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * return 0 means that everythig is correct in our program
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
