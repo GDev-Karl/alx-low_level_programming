78d7dc1bbf78eeeea5e074c109085bf55a4d92f8
Karl <tekamkarl@gmail.com>
2022-10-14 12:01:45 -0700

    sign of a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,7 @@
  * main - our program starts here
  * Return 0 - the end of our program
  */
+
 int main(void)
 {
 	int n;
@@ -14,12 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if (n < 0)
-		printf("%d is negative \n", n);
+		printf("%d is negative\n", n);
 	else if (n == 0)
-		printf("%d is zero \n", n);
+		printf("%d is zero\n", n);
 	else 
-		printf("%d is positive \n", n);
+		printf("%d is positive\n", n);
 
-	/* your code goes there */
 	return (0);
 }
