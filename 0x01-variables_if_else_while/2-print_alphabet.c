7963dba25aa3867c814263c3e54962eee47d2c7f
Karl <tekamkarl@gmail.com>
2022-10-14 13:54:00 -0700

    prints all the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - where our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char lowcase;
+
+	for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
+	{
+		putchar(lowcase); 
+		putchar('\n');
+	}
+	
+	return (0);
+}
