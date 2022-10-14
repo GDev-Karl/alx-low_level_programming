d0ddcef581059ceb32d3187c1d1b8bb91cf95527
Karl <tekamkarl@gmail.com>
2022-10-14 14:45:06 -0700

    print all digit of the base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - where our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+  int;
+
+  for (d = '0'; d <= '9'; d++)
+    putchar(d);
+  putchar('\n');
+
+  return (0);
+}
