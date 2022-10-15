592889eda031b368df6b97193f0282f6e3ca573d
Karl <tekamkarl@gmail.com>
2022-10-15 04:29:07 -0700

    combinaison, numbers and commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - where our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int nbr;
+
+	for (nbr = 0; nbr <= 9; nbr++)
+	{
+		putchar(nbr  '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
