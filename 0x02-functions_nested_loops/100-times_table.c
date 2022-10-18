d540405e026e33ae4f162d789655079997eb4951
Karl <tekamkarl@gmail.com>
2022-10-18 14:48:57 -0700

    times table print

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,44 +9,45 @@
 
 void print_times_table(int n)
 {
-  int x, y, p;
-
-  if (n >= 0 && n <= 14)
-    {
-      for (x = 0; x <= n; x++)
+	int x, y, p;
+	
+	if (n >= 0 && n <= 14)
 	{
-	  for (y = 0; y <= n; y++)
-	    {
-	      p = x * y;
-	      if (p > 99)
+		for (x = 0; x <= n; x++)
 		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar((p / 100) + '0');
-		  _putchar(((p / 10) % 10) + '0');
-		  _putchar((p % 10) + '0');
-		}
-	      else if (p > 9)
-		{
-		  _putchar(',');
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar(((p / 10) % 10) + '0');
-		  _putchar((p % 10) + '0');
-		}
-	      else
-		{
-		  if (y != 0)
-		    {
-		      _putchar(',');
-		      _putchar(' ');
-		      _putchar(' ');
-		      _putchar(' ');
-		    }
-		  _putchar(p + '0');
-		}
-	    }
-	  _putchar('\n');
-	}
-    }
+			for (y = 0; y <= n; y++)
+			{
+				p = x * y;
+		
+				if (p > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10) % 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+				else if (p > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((p / 10) % 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(p + '0');
+				}	
+			}	
+			_putchar('\n');
+		}		
+    	}
 }
