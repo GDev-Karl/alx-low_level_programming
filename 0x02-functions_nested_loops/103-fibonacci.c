4534135f1cd4919b21ff25335389e8c70274ac67
Karl <tekamkarl@gmail.com>
2022-10-18 15:41:16 -0700

    fibonaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <sdtio.h>
+
+/**
+ * main - where our progam starts
+ *
+ * Retrun: 0 success
+ */
+
+int main(void)
+{
+	int i = 0;
+	long a = 1, b = 2, sum = b;
+
+	while ( a+ b < 4000000)
+	{
+		b += a;
+
+		if (b % 2 == 0)
+			sum += b;
+
+		a = b - a;
+
+		i++
+	}
+
+	printf("%ld\n"), sum);
+	return (0);
+}
