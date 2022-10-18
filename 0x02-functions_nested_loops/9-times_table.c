485feff18c4bc115ae1dcecb2f4b047b6b86892b
Karl <tekamkarl@gmail.com>
2022-10-18 13:33:04 -0700

    times tales

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_tables - print the times tables
+ *
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+	int l, c, p, m, d;
+
+	for (l = 0; l <= 9; l++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			p = l * c;
+
+			if (p > 9)
+			{
+				m = p % 10;
+				d = (p -m) / 10;
+
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d + '0');
+				_putchar(m + '0');
+			}
+			else
+			{
+				id (c != 0 )
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+
+				_putchar(p + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
