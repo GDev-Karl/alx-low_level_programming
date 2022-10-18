daea5202502600505abe1630d3199b708fe499d2
Karl <tekamkarl@gmail.com>
2022-10-18 15:14:44 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - whee our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	while (i > 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
