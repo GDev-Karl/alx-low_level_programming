a968c4628d4854628a66ebf6dcef10ac60e3b290
Karl <tekamkarl@gmail.com>
2022-10-18 15:26:00 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - where our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i = 0;
+	long a =1, b = 2;
+
+	while (i < 50)
+	{
+		if (i == 50)
+			printf("%ld", a);
+		else if (i == 0)
+			printf(", %ld", b);
+		else
+		{
+			b += a;
+			a = b - a;
+			printf(", %ld", b);
+		}
+
+		i++;
+	}
+
+	printf("\n");
+	return (0);
+}
