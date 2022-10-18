d208cc012e5eee63916fd2e7999661db95988bff
Karl <tekamkarl@gmail.com>
2022-10-18 04:19:57 -0700

    display _putchar on the screen by using only putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - where our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char *list = "_putchar";
+
+	for (int i = 0; i <= strlen(list); i++)
+		putchar(list[i]);
+
+	putchar('\n');
+
+	return (0);
+}
+
