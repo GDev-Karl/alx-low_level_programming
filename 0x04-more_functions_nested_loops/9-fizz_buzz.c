03df72954f35775c79286a846eea024b00f2f26c
Karl <tekamkarl@gmail.com>
2022-10-20 16:25:52 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * *main - where our program starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+	char *f = "Fizz";
+	char *b = "Buzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s %s", f, b);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
