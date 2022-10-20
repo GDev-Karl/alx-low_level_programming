e67a4358577da703aed4bcc365e14257726122eb
Karl <tekamkarl@gmail.com>
2022-10-20 15:11:44 -0700

    print numbers for 0 to nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers for 0 to 9
+ * 
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
