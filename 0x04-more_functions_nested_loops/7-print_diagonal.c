b271805d517244c128ae3a47829901a7b26f4f88
Karl <tekamkarl@gmail.com>
2022-10-20 16:05:49 -0700

    print diagonal on the screen

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - display a diagonal
+ * @n: parameter
+ *
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
