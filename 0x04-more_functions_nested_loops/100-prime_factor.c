379e010150914fa2365b7edbee3960468277569f
Karl <tekamkarl@gmail.com>
2022-10-20 17:06:11 -0700

    factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -where our progam starts
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	unsigned long it i, n = 612852475143;
+
+	for (i = 3; i < 782849; i = i + 2)
+	{
+		while ((n % i == 0) && (n != i))
+			n = n / i;
+	}
+
+	printf("%lu\n", n);
+
+	return (0);
