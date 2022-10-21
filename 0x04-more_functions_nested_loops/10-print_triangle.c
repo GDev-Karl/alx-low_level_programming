e4c433a83439c5ab220ab875ebc1408e9db0276f
Karl <tekamkarl@gmail.com>
2022-10-20 17:18:47 -0700

    print rectangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,23 +9,24 @@
 
 void print_triangle(int size)
 {
-	int x, y, z;
-
-	if (size <= 0)
+  if (size <= 0)
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      int i, j;
+      for (i = 1; i <= size; i++)
 	{
-		_putchar('\n');
+	  for (j = i; j < size; j++)
+	    {
+	      _putchar(' ');
+	    }
+	  for (j = 1; j <= i; j++)
+	    {
+	      _putchar;
+	    }
+	  _putchar('\n');
 	}
-	else
-	{
-		for (y = 0; y <= (size - 1); y++)
-		{
-			for (x = 0; x < ((size - 1) - y), x++)
-				_putchar(' ');
-
-			for (z = 0; z <= y; z++)
-				_putchar('#');
-
-			_putchar('\n');
-		}
-	}
+    }
 }
