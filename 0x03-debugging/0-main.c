efc9b6be7d5150b430ab60e2e0cd472dec7edf69
Karl <tekamkarl@gmail.com>
2022-10-21 18:52:24 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * main - wher our program starts
+ *main - Entry point
  *
- * Return: 0 success
+ *Return: 0 always
  */
 
 int main(void)
 {
-	int i;
-	i = 0;
-
-	positive_or_negative(i);
-
-	return (0);
+  int i;
+  i = 0;
+  positive_or_negative(i);
+  return(0);
 }
