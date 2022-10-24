ac17fc8f9f7cef66e2afa06869dfd0fac6730202
Karl <tekamkarl@gmail.com>
2022-10-24 11:47:16 -0700

    retun the le of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - len of a string
+ * @s: parameter
+ *
+ * Return: the len of *s
+ *
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (*(s + ))
+		i++;
+
+	return (i);
+}
