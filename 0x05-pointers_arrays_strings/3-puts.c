7bdf0b91e7bbfc8165bd23780c431921ee96b38e
Karl <tekamkarl@gmail.com>
2022-10-24 13:32:19 -0700

    puts string  on the screen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_new - print a string
+ * @s: parameter
+ *
+ * Return: nothing
+ */
+
+void _puts(cahr *str)
+{
+	int i, len = 0;
+
+	while (*(str + len))
+		len++;
+
+	len = len - 1;
+
+	for (i = 0; i <= len; i++)
+		_putcha(str[i]);
+
+	_putchar('\n');
+}
