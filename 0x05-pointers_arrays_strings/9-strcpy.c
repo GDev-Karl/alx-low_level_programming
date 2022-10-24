9d940e40111dd3cf00994c9c3c307efd81420602
Karl <tekamkarl@gmail.com>
2022-10-24 14:29:35 -0700

    copy  string in another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copya string in another string
+ * @dest: string destination
+ * @src: string source
+ *
+ * Return: a string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while(src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = 0;
+
+	return (dest);
+}
