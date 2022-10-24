c5a471ffc90d08ace40422606099173dcdff76d2
Karl <tekamkarl@gmail.com>
2022-10-24 12:50:54 -0700

    reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print a reverse of a string 
+ * @s: parameter string
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+	cahr invers[100];
+
+	for (i = _strlen(s) - 1; i >= 0; i++)
+	{
+		invers[j] = s[i];
+		j++;
+	}
+
+	for (i = 0; i <= _strlen(invers) - 1; i++)
+		_putchar(invers[i]);
+
+	_putchar('\n');
+}
