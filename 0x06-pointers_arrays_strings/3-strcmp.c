f80a0363ea7e1b920879722c9ef25e14a91275b7
Karl <tekamkarl@gmail.com>
2022-10-27 07:23:57 -0700

    compare to strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compae two strings
+ * @s1: first parameter
+ * @s2: second parameter
+ *
+ * Return: an interger
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, v;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			v = s1[i] - s2[i];
+			break;
+		}
+		else
+			v = s1[i] - s2[i];
+		
+		i++;
+	}
+
+	return (v);
+}
