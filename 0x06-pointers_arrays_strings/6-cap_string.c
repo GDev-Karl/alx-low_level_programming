7ef400b47fb11629d97270df95892ec4942e6f93
Karl <tekamkarl@gmail.com>
2022-10-27 08:34:51 -0700

    capitalizes string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes string
+ * @n: string
+ *
+ * Return: string after capitalization
+ */
+
+char *cap_string(char *n)
+{
+	int i, x;
+	int cap = 32;
+	int sep[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - cap;
+
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == sep[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+}
