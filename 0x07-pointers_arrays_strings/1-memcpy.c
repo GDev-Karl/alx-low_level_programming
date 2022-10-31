d47aaa170e39b79da0ae54f1d2f7868b669edb15
Karl <tekamkarl@gmail.com>
2022-10-31 10:23:59 -0700

    copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * @dest: destination
+ * @src: source
+ * @n: byte
+ *
+ * Return: a pointer to an array of char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
