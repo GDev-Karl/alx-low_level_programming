111bb694301db2a7f9ad69381f9e083cfd4f7bb2
Karl <tekamkarl@gmail.com>
2022-10-31 11:09:53 -0700

    cherch a charater in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - checks a character in a string
+ * @s: string 
+ * @c: character
+ *
+ * Return: a pointer or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int  i = 0;
+
+	while (s[i] == c)
+	{
+		if (s[i] ==  c)
+			return (s + i);
+		i++;
+	}
+
+	return '\0';
+}
