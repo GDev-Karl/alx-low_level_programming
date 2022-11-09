ba2c3c93d67c8f89d069421bf7eb1a57a5f10337
Karl <tekamkarl@gmail.com>
2022-11-08 17:50:00 -0800

    creates an array of chars, and initializes it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * crete_array - creates an array of chars, and initializes it
+ * @size: size of array
+ * @c: character
+ *
+ * Return: an aray of chars
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc (sizeof(c) * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0, i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
