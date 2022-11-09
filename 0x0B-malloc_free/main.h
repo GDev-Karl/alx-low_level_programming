3140599f088f2716c09a734b5562ec95972a2006
Karl <tekamkarl@gmail.com>
2022-11-08 18:01:58 -0800

    main header

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,8 @@
-fndef _MAIN_H_
+#ifndef _MAIN_H_
 #define _MAIN_H_
 
 #include <stdio.h>
-#include <sdtlib.h>
+#include <sdlib.h>
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
