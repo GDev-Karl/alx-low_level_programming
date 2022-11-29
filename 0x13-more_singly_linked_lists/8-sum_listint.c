6dcaa02fb7a591110fcc94bea29bdc3bc64c611a
Karl <tekamkarl@gmail.com>
2022-11-29 19:54:02 +0100

    sum of the data of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of the data of a list
+ * @head: head of the list
+ * 
+ * Return: the sum of the data
+ */
+
+int sum_listint(listint_t *head)
+{
+    int sum = 0;
+
+    while(head)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+
+    return (sum)
+}
