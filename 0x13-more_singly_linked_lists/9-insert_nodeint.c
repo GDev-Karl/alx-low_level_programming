24c791021ac735623e8be267ba53b65ef08178fc
Karl <tekamkarl@gmail.com>
2022-11-30 03:30:34 +0100

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,41 +12,36 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
     unsigned int i;
-    listint_t *tmp ;
     listint_t *list;
-
+    listint_t *tmp;
+    
     tmp = *head;
 
-    if(idx != 0)
-    {
-        for(i = 0; i < idx &&  tmp != NULL; i++)
-        {
-            tmp = tmp->next; 
-        }
-    }
+	if (idx != 0)
+	{
+        for (i = 0; i < idx - 1 && tmp != NULL; i++)
+            tmp = tmp->next;
+	}
 
-    if(tmp == NULL && idx != 0)
-        return (NULL);
-        
-    list = malloc(sizeof(listint_t));
-
-    if(!list)
+	if (tmp == NULL && idx != 0)
         return (NULL);
 
-    list->n = n;
+	list = malloc(sizeof(listint_t));
+	if (list == NULL)
+        return (NULL);
 
-    if (idx == 0)
-    {
-        
-        list->next = *head;
-        *head = list;
-        
-    }
-    else
-    {
-        list->next = tmp;
-        tmp->next = list;
-    }
+	list->n = n;
 
-    return (list);
+	if (idx == 0)
+	{
+		list->next = *head;
+		*head = list;
+	}
+	else
+	{
+		list->next = tmp->next;
+		tmp->next = list;
+	}
+
+	return (list);
 }
