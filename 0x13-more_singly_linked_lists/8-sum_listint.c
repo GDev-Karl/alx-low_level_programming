49fcee22372f85d3d2b4f9473638e954d820ddb1
Karl <tekamkarl@gmail.com>
2022-11-30 02:30:05 +0100

    sum of the data of a list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,13 +10,12 @@
 int sum_listint(listint_t *head)
 {
     int sum = 0;
-    listint_t *tmp = head;
 
-    while(tmp)
+    while(head != NULL)
     {
-        sum += tmp->n;
-        tmp = tmp->next;
+        sum += head->n;
+        head = head->next;
     }
 
-    return (sum)
+    return (sum);
 }
