21b25006d964f16b38bd9ed5a06412566830017c
Karl <tekamkarl@gmail.com>
2023-04-06 22:35:20 +0100

    prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_puts_recursion - prints a string
  *@a: pointer block of memory to fill
@@ -7,13 +8,13 @@
 
 void _puts_recursion(char *a)
 {
-  if (*a == '\0')
+  if(*a == '\0')
   {
     _putchar('\n');
   }
   else
   {
-    _putchar (*a);
-    _puts_recursion(a + 1);
+    _putchar(*a);
+    _putchar(a+1);
   }
 }
