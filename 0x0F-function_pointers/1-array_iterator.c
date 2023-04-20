8e9edbe3ec8619133020949d7f0815556bdd684f
Karl <tekamkarl@gmail.com>
2023-04-20 02:46:47 +0100

    print all elements in an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print all elements in an array
+ * @array: array of elements
+ * @size: size of the array
+ * @action: pointer to a function
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    int i;
+
+    if(!array || !action)
+        return;
+
+    for (i = 0; i < size; i++)
+        action(array[i]);
+}
