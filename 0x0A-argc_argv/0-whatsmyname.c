2d5528346a98d5c3dd06d24fe85b54aa25dd0fc3
Karl <tekamkarl@gmail.com>
2023-08-10 02:18:33 +0200

    program that prints its name,

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * 
  * Return: 0 for success
 */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
-    printf("%s \n", *argv);
-    return (0);
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
