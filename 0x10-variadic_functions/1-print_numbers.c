2dc646dddea5e2c42f6bd6d9e25f2d9b657283a8
Karl <tekamkarl@gmail.com>
2023-08-17 14:24:25 +0200

    prints numbers separate by a string

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,24 +3,24 @@
 /**
  * print_numbers - prints numbers.
  * @separator: string to be printed between numbers
- * @n: number of integers
+ * @n: number of integers to print
  *
  * Return: Nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list valist;
+	va_list ptr_valist;
 	unsigned int i;
 
-	va_start(valist, n);
+	va_start(ptr_valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(valist, int));
+		printf("%d", va_arg(ptr_valist, int));
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 
+	va_end(ptr_valist);
 	printf("\n");
-	va_end(valist);
 }
