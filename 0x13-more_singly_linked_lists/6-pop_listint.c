8db5671c9fd3c40e98e0f69fc2fcffb5f498277d
Karl <tekamkarl@gmail.com>
2023-08-30 01:50:06 +0200

    delete the head an return his data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,22 +3,22 @@
 /**
  * pop_listint - delete the head an return his data
  * @head: head of a list
- * 
  * Return: a data n
  */
 
 int pop_listint(listint_t **head)
 {
-    listint_t *tmp;
-    int n;
+	listint_t *tmp;
+	int n;
 
-    if(!head || !(*head))
-        return (0);
+	if (!head || !(*head))
+		return (0);
 
-    n = (*head)->n;
-    tmp = (*head)->next;
-    free(*head);
-    *head = tmp;
-    return (n);
+	n = (*head)->n;
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+
+	return (n);
 
 }
