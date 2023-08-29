e1235e5887ff9ba769e6c7e977cfd9b0cbbbef22
Karl <tekamkarl@gmail.com>
2023-08-30 01:28:14 +0200

    header file for the singly linked listt

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef __LISTS__
+#define __LISTS__
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -30,3 +33,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
+#endif
