af45da97c0135fdc1162cac4a1a7ed011b870077
Karl <tekamkarl@gmail.com>
2023-08-30 01:39:38 +0200

    len of a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,20 +2,20 @@
 
 /**
  * listint_len - return  the len of a list
- * 
+ *
  * @h: list to calculate len
  * Return: size of h
  */
 
 size_t listint_len(const listint_t *h)
 {
-    size_t size = 0;
+	size_t size = 0;
 
-    while(h != NULL)
-    {
-        size++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		size++;
+		h = h->next;
+	}
 
-    return (size);
+	return (size);
 }
