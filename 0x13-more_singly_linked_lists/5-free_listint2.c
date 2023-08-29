0f23b16fb032a7aa8cb47586ee7201c3fcaf271b
Karl <tekamkarl@gmail.com>
2023-08-30 01:49:54 +0200

    free a list a put the head tu NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,22 +3,22 @@
 /**
  * free_listint2 - free a list a put the head tu NULL
  * @head: the head of a list
- * 
  * Return: nothing
  */
 
 void free_listint2(listint_t **head)
 {
-    listint_t *tmp;
+	listint_t *tmp;
 
-    if (head == NULL)
-        return;
-        
-    while(*head)
-    {
-        tmp = (*head)->next;
-        free((*head));
-        *head = tmp;
-    }
-    *head = NULL;
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free((*head));
+		*head = tmp;
+	}
+
+	*head = NULL;
 }
