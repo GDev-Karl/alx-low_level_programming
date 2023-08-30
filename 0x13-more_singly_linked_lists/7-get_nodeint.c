ff7e6902a28fcb99837020e281d3c429ac347ead
Karl <tekamkarl@gmail.com>
2023-08-30 02:01:57 +0200

    Get the nodeint at index object

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,16 +4,15 @@
  * get_nodeint_at_index - Get the nodeint at index object
  * @head: head of the list
  * @index: index of the node
- * 
- * Return: listint_t* 
+ * Return: the head of the list
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    for(i = 0; i < index && head != NULL; i++)
-        head = head->next;
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
 
-    return (head);
+	return (head);
 }
