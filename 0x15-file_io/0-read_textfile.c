19a5703275e3e66b34db43dd8ed86e34f729908f
Karl <tekamkarl@gmail.com>
2023-09-18 10:05:23 +0200

    reads a text file and prints it

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,56 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it
+ * read_textfile - reads a text file and write it
  * @filename: text file to print
  * @letters: number of letters to be read
  * Return: the content of file or NULL
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
 	char *buffer;
-	ssize_t file;
-	ssize_t write;
-	ssize_t read;
+	ssize_t bytes_read, bytes_written;
 
-	file = open(filename, O_RDONLY);
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	read = read(file, buffer, letters);
-	write = write(STDOUT_FILENO, buffer, t);
+	buffer = (char *)malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
 	free(buffer);
-	close(file);
-	return (write);
+	close(fd);
+	return (bytes_written);
 }
